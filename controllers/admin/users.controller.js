58fc4caab6385a0175bafe722d95ffd20807f065
tjtarunkumar <akash.webteasor@gmail.com>
2022-10-20 11:14:39 +0530

    changes

--- controllers/admin/users.controller.js
+++ controllers/admin/users.controller.js
@@ -1,5 +1,9 @@
+const userRole = require('../../models/user_role.model')
+
 module.exports = {
-    'index' : (req,res)=>{
-        res.render('admin/users/add',{title : 'Users Add Page',page_title_1:'Add Users Page' ,layout:'dashboard_layout', isUsers: true})
+    'index' : async (req,res)=>{
+        const user_role = await userRole.find({})
+       
+        res.render('admin/users/add',{title : 'Users Add Page',page_title_1:'Add Users Page' ,layout:'dashboard_layout', user_role:user_role ,isUsers: true})
     }
 }
--- routes/admin/users.js
+++ routes/admin/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const usersController = require('../../controllers/admin/users.controller')
+// const { chec } = require('express-validator')
 
 router.get('/',usersController.index)
 
