8eace6e835445210a4a91b826eb373b15cb55659
tjtarunkumar <akash.webteasor@gmail.com>
2022-10-20 18:02:45 +0530

    user creation done , validation in-progress

--- controllers/admin/users.controller.js
+++ controllers/admin/users.controller.js
@@ -1,9 +1,56 @@
 const userRole = require('../../models/user_role.model')
+const states = require('../../models/states.model')
+const UsersData = require('../../models/users_data.model')
+const { Schema } = require('mongoose')
+const { validationResult } = require('express-validator')
 
 module.exports = {
     'index' : async (req,res)=>{
         const user_role = await userRole.find({})
-       
-        res.render('admin/users/add',{title : 'Users Add Page',page_title_1:'Add Users Page' ,layout:'dashboard_layout', user_role:user_role ,isUsers: true})
+        const data = await states.find()
+    
+        res.render('admin/users/add',{title : 'Users Add Page',page_title_1:'Add Users Page' ,layout:'dashboard_layout', user_role:user_role , data:data , isUsers: true})
+    },
+
+    'state_data' : async (req,res)=>{
+   
+        const data = await states.find({_id:req.body.state_id}).select('districts')
+        
+        res.send(200,data[0].districts)
+    },
+    
+    'add_user' : async (req,res)=>{
+
+        const error = validationResult(req); 
+
+        if(error.errors.length > 0){
+            req.flash("form1",[{
+                "first_name" : req.body.first_name ,
+                "last_name" : req.body.last_name ,
+                "email" : req.body.email ,
+                "mobile_number" : req.body.mobile_number ,
+                "user_role" : req.body.user_role ,
+                "state" : req.body.state ,
+                "city" : req.body.city,
+                "description" : req.body.description
+            }]);
+            console.log(error.errors);
+            req.flash('product_error',error.errors)
+            return res.redirect('/admin/users')
+        }
+
+        await UsersData.create({
+            first_name : req.body.first_name ,
+            last_name : req.body.last_name ,
+            email : req.body.email ,
+            mobile : req.body.mobile_number ,
+            user_role : req.body.user_role ,
+            state : req.body.state ,
+            city : req.body.city ,
+            description : req.body.description 
+
+        })
+
+        return res.redirect('/admin/dashboard')
     }
 }
