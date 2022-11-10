133d4fec4800082c189870562ca7c255e559f75f
tjtarunkumar <akash.webteasor@gmail.com>
2022-11-10 09:59:59 +0530

    changes

--- controllers/admin/plots.controller.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'index' : (req,res)=>{
-        res.render('admin/plots/list',{title:'Plots', page_title_1:'Plots Page',page_title_2:'Plots Listing',isPlot : true , layout:'dashboard_layout'})
-    },
-    'verification': (req,res)=>{
-
-    }
-}   
--- routes/admin/plots.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const plotsController = require('../../controllers/admin/plots.controller')
-
-router.get('/',plotsController.index)
-
-module.exports = router
