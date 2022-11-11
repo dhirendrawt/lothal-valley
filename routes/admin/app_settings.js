986c92adcd55672c1d6ac8f1f6492aeb95cac3c4
tjtarunkumar <jyotitarun280@gmail.com>
2022-11-11 17:38:30 +0530

    some changege in home section and about section and workin on app setting in admin section

---
+++ routes/admin/app_settings.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+const appSettingsController = require('../../controllers/admin/app_settings.controller');
+const { check } = require('express-validator');
+const multer  = require('multer');
+const upload = require('../../multer');
+
+router.get('/',appSettingsController.index);
+router.get('/add',appSettingsController.create);
+router.post('/add',upload.single('logo'),[
+    check('mobile').not().isEmpty(),
+    check('email').not().isEmpty(),
+    check('address').not().isEmpty(),
+    check('state').not().isEmpty(),
+    check('city').not().isEmpty(),
+    check('pin_code').not().isEmpty(),
+    check('instagram').not().isEmpty(),
+    check('facebook').not().isEmpty(),
+    check('linkedin').not().isEmpty(),
+    check('twitter').not().isEmpty(),
+    check('description').not().isEmpty(),
+]
+,appSettingsController.add);
+router.get('/edit/:id',appSettingsController.create_edit);
+router.post('/edit/:id',appSettingsController.update);
+
+module.exports = router;
