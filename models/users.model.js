9217f9a1b7011f27b795f5bf06b3f22e166424e9
akshaywebteasor <akshay.webteasor@gmail.com>
2022-11-13 18:04:23 +0530

    signup and chenges in login concept..

--- models/users.model.js
+++ models/users.model.js
@@ -4,7 +4,16 @@ const Schema = mongoose.Schema
 //const objectID = Schema.ObjectID
 
 const UsersSchema = new Schema({
+    name : String,
+    role : {
+        type : Schema.Types.ObjectId,
+        ref : 'User_role',
+        default : "636cd13175f1a5c49d1ec767"
+    },
+    mobile_no : String,
+    country_code : { type: String , default: "+91" },
     email : String,
+    agree :Number,
     password : String,
     created_at : { type:Date , default: Date.now }
 })
